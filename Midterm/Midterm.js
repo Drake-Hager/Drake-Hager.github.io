3af36ca4a3c44e2dab01e6a32f07b80e1f3628bc
Drake Hager <Drake_hager@insideranken.org>
2023-11-20 11:33:53 -0600

    Midterm, Portfolio, and github folders

--- Midterm/Midterm.js
+++ Midterm/Midterm.js
@@ -1,20 +1,26 @@
 "Use Strict"
 
-
-
-
+const $ = selector => document.querySelector(selector);
+let errormessage = ""
 function submit(){
-var name = document.getElementById(`name`);
-var phone = document.getElementById(`phonenumber`);
-var email = document.getElementById(`email`);
-var company = document.getElementById(`company`)
-var message = document.getElementById(`message`);
+var name = $(`#name`);
+var phone = $(`#phonenumber`);
+var email = $(`#email`);
+var company = $(`#company`)
+var message = $(`#message`);
 
-var name2 = document.getElementById(`Name`);
-var phone2 = document.getElementById(`Phone`);
-var email2 = document.getElementById(`Email`);
-var company2 = document.getElementById(`Company`);
-var message2 = document.getElementById(`message`);
+var name2 = $(`#Name`);
+var phone2 = $(`#Phone`);
+var email2 = $(`#Email`);
+var company2 = $(`#Company`);
+var message2 = $(`#Message`);
+if(name2 = ""){errormessage += "You must insert a name"};
+if(phone2 = ""){errormessage += "You must insert a phone number"};
+if(email2 = ""){errormessage += "You must insert an email address"};
+if(name=""){errormessage += "You must insert a name"};
+
+
+$("#span1").textcontent = errormessage;
 
 name2.value = name.value;
 phone2.value = phone.value;
